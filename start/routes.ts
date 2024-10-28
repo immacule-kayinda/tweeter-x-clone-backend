3e972b50c6105b3a76ab09e1d416f528cfef9594
Immacule Kayinda <immakaymw@gmail.com>
2024-10-28 10:47:44 +0100

    userController route and protect routes

--- start/routes.ts
+++ start/routes.ts
@@ -8,14 +8,23 @@
 */
 
 const TweetsController = () => import('#controllers/tweets_controller')
+const UsersController = () => import('#controllers/users_controller')
 import router from '@adonisjs/core/services/router'
+import { middleware } from './kernel.js'
 
 router.get('/', async () => {
   return {
     hello: 'world',
   }
 })
-router.get('api/tweets', [TweetsController, 'index'])
-router.get('api/tweets/:tweetId', [TweetsController, 'show'])
-router.get('api/tweets/user/:username', [TweetsController, 'indexByUsername'])
-router.post('api/tweets', [TweetsController, 'store'])
+router
+  .group(() => {
+    router.get('api/tweets', [TweetsController, 'index'])
+    router.get('api/tweets/:tweetId', [TweetsController, 'show'])
+    router.get('api/tweets/user/:username', [TweetsController, 'indexByUsername'])
+    router.post('api/tweets', [TweetsController, 'store'])
+  })
+  .use(middleware.auth())
+
+router.post('/api/users', [UsersController, 'store']).use(middleware.guest())
+router.post('/api/users/register', [UsersController, 'register']).use(middleware.guest())
